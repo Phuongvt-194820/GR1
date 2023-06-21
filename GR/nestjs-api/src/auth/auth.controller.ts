055f9175202e7f3296dbb0f115839e947ac11bd2
Vu Thi Phuong <phuong.vt194820@sis.hust.edu.vn>
2023-06-21 14:06:23 +0700

    update

---
+++ GR/nestjs-api/src/auth/auth.controller.ts
@@ -0,0 +1,63 @@
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import * as moment from 'moment';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
+
+class TodayResponse {
+    @ApiProperty({ example: '20-06-2023', description: 'The current date' })
+    date: string;
+}
+@ApiTags('Today')
+@Controller('auth')
+
+class CreateUserDto{
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    age: number;
+
+    @ApiProperty()
+    email: string;
+}
+
+export class AuthController{
+    //auth service is automatically created when initializing the controller
+    constructor(private authService: AuthService){
+
+    }
+    //some requests from client
+    //POST: .../auth/register
+    @Post("/register") //register a new user
+    register(@Body() registrationData: any){
+        return{
+            message: "Register an user",
+            data: registrationData,
+        }
+    }
+    //POST: .../auth/login
+    @Post("/login")
+    login(@Body() loginData: any){
+        return{
+            message: "Login successfully",
+            token: 'abc123',
+        }
+    }
+
+    @Post("/aboutus")
+    aboutus(){
+        return{
+            name: "Vu Thi Phuong",
+            age: "22",
+            mssv: "20194820",
+            email:"phuong.vt1909@gmail.com",
+        }
+    }
+
+    @Get('/today')
+    getToday(): TodayResponse {
+      const today = moment().format('DD-MM-YYYY');
+      return { date: today };
+    }
+
+}
